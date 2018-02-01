e5a0c4c554d5dad8c4990ca687b259cde573e36b
Hugh Davenport <hugh@davenport.net.nz>
2018-02-02 11:16:51 +1300

    Add patreon support to script

---
+++ patreon-cookie-example.sh
@@ -0,0 +1,2 @@
+# Add these fields from a logged in patreon session, we could probably script the login as well, but meh
+COOKIE="__cfduid=...; patreon_device_id=...; __ssid=...; group_id=...; __stripe_mid=...; amplitude_idpatreon.com=...; ki_t=...; ki_r=...; __unam=...; session_id=..."
