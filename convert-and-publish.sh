a9a650794828bdb6832f21288e9eafe9bb6e7958
The Bus Driver <github@thebusfactor.party>
2018-02-07 18:28:11 +1300

    add dependency on jq to script

--- convert-and-publish.sh
+++ convert-and-publish.sh
@@ -44,6 +44,12 @@ if [ ! -e ~/.aws/credentials ]; then
 	exit
 fi
 
+JQ=$(which jq 2>/dev/null)
+if [ -z ${JQ} ]; then
+	echo "Couldn't find jq, install it"
+	exit
+fi
+
 if [ -z "${1}" -o -z "${2}" ]; then
 	echo "Usage: $0 filename description [silence, default: 00:00]"
 	echo "Example:"
@@ -103,10 +109,10 @@ cd -
 source $(dirname ${0})/patreon-cookie.sh
 
 CSRFJSON=$(curl 'https://www.patreon.com/REST/auth/CSRFTicket' -H 'cookie: '"${COOKIE}" --compressed)
-CSRF=$(echo "${CSRFJSON}" | jq .token | tr -d '"')
+CSRF=$(echo "${CSRFJSON}" | ${JQ} .token | tr -d '"')
 
 POSTJSON=$(curl 'https://www.patreon.com/api/posts?include=user_defined_tags.null%2Ccampaign.creator.null%2Ccampaign.rewards.campaign.null%2Ccampaign.rewards.creator.null&fields\[post\]=post_type%2Cmin_cents_pledged_to_view&fields\[campaign\]=is_monthly&fields\[reward\]=amount_cents%2Ctitle&fields\[user\]=\[\]&fields\[post_tag\]=value&json-api-version=1.0' -H 'x-csrf-signature: '"${CSRF}" -H 'cookie: '"${COOKIE}" -H 'content-type: application/vnd.api+json' --data-binary '{"data":{"type":"post","attributes":{"post_type":"video_embed"}}}' --compressed)
-POSTID=$(echo "${POSTJSON}" | jq .data.id | tr -d '"')
+POSTID=$(echo "${POSTJSON}" | ${JQ} .data.id | tr -d '"')
 
 curl 'https://www.patreon.com/api/posts/'"${POSTID}"'?include=user.null%2Cattachments.null%2Cuser_defined_tags.null%2Ccampaign.earnings_visibility%2Ccampaign.rewards.null%2Cpoll&fields\[post\]=category%2Ccents_pledged_at_creation%2Cchange_visibility_at%2Ccomment_count%2Ccontent%2Ccreated_at%2Ccurrent_user_can_delete%2Ccurrent_user_can_view%2Ccurrent_user_has_liked%2Cdeleted_at%2Cearly_access_min_cents%2Cedit_url%2Cedited_at%2Cearly_access_min_cents%2Cembed%2Cimage%2Cis_automated_monthly_charge%2Cis_paid%2Clike_count%2Cmin_cents_pledged_to_view%2Cnum_pushable_users%2Cpatreon_url%2Cpatron_count%2Cpledge_url%2Cpost_file%2Cpost_type%2Cpublished_at%2Cscheduled_for%2Cthumbnail%2Ctitle%2Curl%2Cwas_posted_by_campaign_owner&json-api-version=1.0' -X PATCH -H 'x-csrf-signature: '"${CSRF}" -H 'cookie: '"${COOKIE}" -H 'content-type: application/vnd.api+json' --data-binary '{"data":{"type":"post","attributes":{"content":"<p>'"${DESCRIPTION}"'</p>","post_type":"video_embed","is_paid":true,"min_cents_pledged_to_view":0,"title":"The Bus Factor - Episode '"${EPNUMBER}"'","embed":{"description":"'"${DESCRIPTION}"'\n\nFind us on https://thebusfactor.party, and consider joining our patreon at https://patreon.com/thebusfactor","provider":"YouTube","provider_url":"https://www.youtube.com/","html":"\n<iframe\n    src=\"https://www.youtube.com/embed/'"${YOUTUBEID}"'\"\n    class=\"embedly-embed\"\n    width=\"854\"\n    height=\"480\"\n    frameborder=\"0\"\n    scrolling=\"no\"\n    allowfullscreen\n>\n</iframe>\n","url":"https://youtu.be/'"${YOUTUBEID}"'","subject":"maxresdefault.jpg"},"thumbnail":{"height":720,"url":"https://i.ytimg.com/vi/'"${YOUTUBEID}"'/maxresdefault.jpg","width":1280,"index":0},"tags":{"publish":true}},"relationships":{"post_tag":{"data":{"type":"post_tag","id":"user_defined;technology"}},"user_defined_tags":{"data":[{"id":"user_defined;bus","type":"post_tag"},{"id":"user_defined;infosec","type":"post_tag"},{"id":"user_defined;technology","type":"post_tag"}]}}},"included":[{"type":"post_tag","id":"user_defined;bus","attributes":{"value":"bus","cardinality":1}},{"type":"post_tag","id":"user_defined;infosec","attributes":{"value":"infosec","cardinality":1}},{"type":"post_tag","id":"user_defined;technology","attributes":{"value":"technology","cardinality":1}}]}' --compressed
 unset COOKIE
